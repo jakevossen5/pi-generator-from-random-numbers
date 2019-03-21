0c66942865f4329a40e55731a490f33ba305e6ef
= <jake@vossen.dev>
2019-03-20 19:21:16 -0600

    basic senario, not getting right number

--- main.rs
+++ main.rs
@@ -7,51 +7,81 @@ use rand::Rng;
 use std::thread;
 fn main() {
 
-    let num_runs: i128 = std::i128::MAX;
-    let max_number: i128 =std::i128::MAX;
+    let num_runs: i128 = 10000;//std::i128::MAX;
+    let max_number: i128 =10000;//std::i128::MAX;
     // let mut co_primes: i128 = 0;
     let co_primes = Arc::new(Mutex::new(0));
     let threads: i32 = 4;
 
-    // let (tx, rx) = mpsc::channel();
 
-    for _core in 1..threads {
-        let co_primes = Arc::clone(&co_primes);
-        thread::spawn( move || {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
             for _x in 0..10000 {
-                println!("am I even doing anything??");
                 let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
                 let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
                 if num_1.gcd(&num_2) == 1 {
-                    let mut num = co_primes.lock().unwrap();
                     *num +=1;
-                    println!("{}", (_x as f64/ num_runs as f64) * 100.0)
-                        
                 }
             }
-
-            //tx.send(co_primes).unwrap();
         });
+        handles.push(handle);
     }
 
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+    let mut c = *counter.lock().unwrap();
+
+    let mut pi: f64 = (6.0/(c as f64 / num_runs as f64)).sqrt();
+    println!("pi: {}", pi);
+
+
     
-    // for received in rx {
-        // println!("Got: {}", received);
-        // let co_primes = co_primes + received;
+}    
+
+    // let (tx, rx) = mpsc::channel();
+
+    // for _core in 1..threads {
+    //     let co_primes = Arc::clone(&co_primes);
+    //     thread::spawn( move || {
+    //         for _x in 0..10000 {
+    //             println!("am I even doing anything??");
+    //             let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
+    //             let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
+    //             if num_1.gcd(&num_2) == 1 {
+    //                 let mut num = co_primes.lock().unwrap();
+    //                 *num +=1;
+    //                 println!("{}", (_x as f64/ num_runs as f64) * 100.0)
+                        
+    //             }
+    //         }
+
+    //         //tx.send(co_primes).unwrap();
+    //     });
     // }
-    // co_primes.lock().unwrap();
-    let co_primes = Arc::clone(&co_primes);
-    let mut num = co_primes.lock().unwrap();
-
-    println!("Co primes: {}", num);
- 
-    // co_primes.lock().unwrap();   
-    // let pi: f64 = (6.0/(co_primes as f64 / num_runs as f64)).sqrt();
-    // println!("pi: {}", pi);
 
     
-}
+    // // for received in rx {
+    //     // println!("Got: {}", received);
+    //     // let co_primes = co_primes + received;
+    // // }
+    // // co_primes.lock().unwrap();
+    // let co_primes = Arc::clone(&co_primes);
+    // let mut num = co_primes.lock().unwrap();
 
+    // println!("Co primes: {}", num);
+ 
+    // // co_primes.lock().unwrap();   
+    // // let pi: f64 = (6.0/(co_primes as f64 / num_runs as f64)).sqrt();
+    // // println!("pi: {}", pi);
 
 // From here: https://doc.rust-lang.org/std/ops/trait.Div.html
 // fn gcd(x: i128, y: i128) -> i128 {
