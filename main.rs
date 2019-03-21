49a7cc583d28fa82d361737dce8e87d1495f01dc
= <jake@vossen.dev>
2019-03-20 19:16:11 -0600

    frusterated, going to go into copypaste mode

--- main.rs
+++ main.rs
@@ -1,50 +1,53 @@
 extern crate rand;
 extern crate num;
 
+use std::sync::{Mutex, Arc};
 use num::Integer;
 use rand::Rng;
 use std::thread;
-use std::sync::mpsc;
 fn main() {
 
     let num_runs: i128 = std::i128::MAX;
     let max_number: i128 =std::i128::MAX;
-    let mut co_primes: i128 = 0;
+    // let mut co_primes: i128 = 0;
+    let co_primes = Arc::new(Mutex::new(0));
+    let threads: i32 = 4;
 
-    let (tx1, rx1) = mpsc::channel();
-    let (tx2, rx2) = mpsc::channel();
-    thread::spawn( move || {
-        for _x in 0..num_runs/2 {
-            let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
-            let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
-            if num_1.gcd(&num_2) == 1 {
-                co_primes = co_primes + 1;
-                println!("{}", (_x as f64/ num_runs as f64) * 100.0)
+    // let (tx, rx) = mpsc::channel();
+
+    for _core in 1..threads {
+        let co_primes = Arc::clone(&co_primes);
+        thread::spawn( move || {
+            for _x in 0..10000 {
+                println!("am I even doing anything??");
+                let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
+                let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
+                if num_1.gcd(&num_2) == 1 {
+                    let mut num = co_primes.lock().unwrap();
+                    *num +=1;
+                    println!("{}", (_x as f64/ num_runs as f64) * 100.0)
+                        
+                }
             }
-        }
 
-        tx1.send(co_primes).unwrap();
-    });
+            //tx.send(co_primes).unwrap();
+        });
+    }
 
-    thread::spawn( move || {
-        for _x in num_runs/2..num_runs {
-            let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
-            let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
-            if num_1.gcd(&num_2) == 1 {
-                co_primes = co_primes + 1;
-                println!("{}", (_x as f64/ num_runs as f64) * 100.0)
-            }
-        }
+    
+    // for received in rx {
+        // println!("Got: {}", received);
+        // let co_primes = co_primes + received;
+    // }
+    // co_primes.lock().unwrap();
+    let co_primes = Arc::clone(&co_primes);
+    let mut num = co_primes.lock().unwrap();
 
-        tx2.send(co_primes).unwrap();
-    });
-
-    let co_primes = co_primes + rx1.recv().unwrap();
-    let co_primes = co_primes + rx2.recv().unwrap();
-    println!("Co primes: {}", co_primes);
-
-    let pi: f64 = (6.0/(co_primes as f64 / num_runs as f64)).sqrt();
-    println!("pi: {}", pi);
+    println!("Co primes: {}", num);
+ 
+    // co_primes.lock().unwrap();   
+    // let pi: f64 = (6.0/(co_primes as f64 / num_runs as f64)).sqrt();
+    // println!("pi: {}", pi);
 
     
 }
