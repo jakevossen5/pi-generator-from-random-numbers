eb93e0ced161267ad0ac7a2f037a4cc7defe90ad
= <jake@vossen.dev>
2019-03-20 23:46:16 -0600

    cleaned things up, got rid of some warnings

--- main.rs
+++ main.rs
@@ -11,27 +11,28 @@ static GLOBAL_THREAD_COUNT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 fn main() {
 
-    let num_runs: i128 = std::i128::MAX;
-    let max_number: i128 = 100000;//std::i128::MAX;
+    let num_runs: i128 = std::i16::MAX as i128;
+    let max_number: i128 = std::i128::MAX;
     // let mut co_primes: i128 = 0;
-    let co_primes = Arc::new(Mutex::new(0));
+    // let co_primes = Arc::new(Mutex::new(0));
+
     let threads: i32 = 100;
 
 
-    let counter = Arc::new(Mutex::new(0));
+    let co_primes = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     // second number is the number of threads
-    for i in 0..threads {
+    for _i in 0..threads {
         GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
-        let counter = Arc::clone(&counter);
+        let co_primes = Arc::clone(&co_primes);
         let handle = thread::spawn(move || {
             for _x in 0..num_runs/((threads) as i128) {
                 let num_1: i128 = rand::thread_rng().gen_range(1, max_number);
                 let num_2: i128 = rand::thread_rng().gen_range(1, max_number);
                 if num_1.gcd(&num_2) == 1 {
                     // println!("{}-th thread reporting", i);
-                    let mut num = counter.lock().unwrap();
+                    let mut num = co_primes.lock().unwrap();
                     *num +=1;
                     // println!("{}", (_x as f64 / ((num_runs as f64) / ((threads) as f64))) * 100.0)
                 }
@@ -47,11 +48,11 @@ fn main() {
     for handle in handles {
         handle.join().unwrap();
     }
+    let c = *co_primes.lock().unwrap();
+    println!("Result: {}", c);
 
-    println!("Result: {}", *counter.lock().unwrap());
-    let mut c = *counter.lock().unwrap();
 
-    let mut pi: f64 = (6.0/(c as f64 / num_runs as f64)).sqrt();
+    let pi: f64 = (6.0/(c as f64 / num_runs as f64)).sqrt();
     println!("pi: {}", pi);
 
 
